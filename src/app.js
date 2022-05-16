3d220a948bf5af3ccdbb5220f792640eabd64376
DevMarioNan <dev.mario.nan@gmail.com>
2022-05-16 03:53:20 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
+
 hbs.registerPartials(path.join(__dirname,'../templates/partials'))
 app.set('views', path.join(__dirname,'../templates/views'))
 app.set('view engine', 'hbs');
@@ -96,6 +98,6 @@ app.get('*',(req,res)=>{
     });
 });
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000');
+app.listen(port,()=>{
+    console.log('server is up on port ' + port);
 });
