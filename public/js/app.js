943c7ee8dce8e4511c8de908f5a62c5f88c1a217
DevMarioNan <dev.mario.nan@gmail.com>
2022-05-16 03:12:12 +0200

    editing the client js file

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,6 @@ const weatherForm = document.querySelector('form');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
-
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault();
     const value = weatherForm.querySelector('input').value
